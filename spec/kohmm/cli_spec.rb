1f448b913c5bf4d56ee793aeca9212fc70e21b6d
Takuya Aramaki <aramaki@kuicr.kyoto-u.ac.jp>
2018-09-21 20:27:14 +0900

    modify cli_spec.rb

--- spec/kohmm/cli_spec.rb
+++ spec/kohmm/cli_spec.rb
@@ -1,59 +1,54 @@
-BRSpec.describe KOHMM::CLI do
+RSpec.describe KOHMM::CLI do
   describe '.run' do
-    context 'with -h or --help option' do
-      it 'prints help message' do
-        %w[-h --help].each do |option|
-          expect { KOHMM::CLI.run([option]) }.to output(/\AUsage/i).to_stdout
-        end
-      end
+    subject { KOHMM::CLI.run(options) }
+    let(:options) { ["query"] }
 
-      it 'terminates the script' do
+    before do
+      allow(KOHMM::Executor).to receive(:execute)
+    end
+
+    context 'with -h or --help option' do
+      it 'prints help message and exit' do
         %w[-h --help].each do |option|
-          expect { KOHMM::CLI.run([option]) }.to exit_script.successfully
+          expect { KOHMM::CLI.run([option]) }.to output(/\AUsage/i).to_stdout.and exit_script.successfully
         end
       end
     end
 
     context 'without help option' do
-      it 'invokes KOHMM::Executor.execute' do
-        expect(KOHMM::Executor).to receive(:execute)
-        KOHMM::CLI.run(['query'])
-      end
-    end
-
-    context 'with invalid options' do
-      before do
-        allow_any_instance_of(KOHMM::CLI::OptionParser).to(
-          receive(:parse!).and_raise(::OptionParser::ParseError)
-        )
+      context 'with vaild arguments' do
+        it 'invokes KOHMM::Executor.execute' do
+          expect(KOHMM::Executor).to receive(:execute)
+          KOHMM::CLI.run(options)
+        end
       end
 
-      it 'shows usage to stderr' do
-        expect { KOHMM::CLI.run(['query']) }.to output(/Usage/i).to_stderr
+      context 'with invalid options' do
+        before do
+          allow_any_instance_of(KOHMM::CLI::OptionParser).to(
+            receive(:parse!).and_raise(::OptionParser::ParseError)
+            )
+        end
+
+        it 'shows usage to stderr and exit with error' do
+          expect { subject }.to output(/Usage/i).to_stderr.and exit_script.unsuccessfully
+        end
       end
 
-      it 'exits with exit status > 0' do
-        expect { KOHMM::CLI.run(['query']) }.to exit_script.unsuccessfully
-      end
-    end
+      context 'without query' do
+        let(:options) { %w[-o out_file] }
 
-    context 'without query' do
-      it 'shows an error message to stderr and' do
-        expect { KOHMM::CLI.run(%w[-o out_file]) }.to output(/specify a query/i).to_stderr
+        it 'shows an error message to stderr and exit with error' do
+          expect { subject }.to output(/specify a query/i).to_stderr.and exit_script.unsuccessfully
+        end
       end
 
-      it 'exits with exit status > 0' do
-        expect { KOHMM::CLI.run(%w[-o out_file]) }.to exit_script.unsuccessfully
-      end
-    end
+      context 'with too many args' do
+        let(:options) { %w[file_a file_b] }
 
-    context 'with too many args' do
-      it 'shows an error message to stderr' do
-        expect { KOHMM::CLI.run(%w[-o out_file]) }.to output(/too many/i).to_stderr
-      end
-
-      it 'exits with exit status > 0' do
-        expect { KOHMM::CLI.run(%w[-o out_file]) }.to exit_script.unsuccessfully
+        it 'shows an error message to stderr and exit with error' do
+          expect { subject }.to output(/too many/i).to_stderr.and exit_script.unsuccessfully
+        end
       end
     end
   end
