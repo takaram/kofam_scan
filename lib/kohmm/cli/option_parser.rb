31f39883ff4ed6a195fd37b31b2860e957bf69c6
Takuya Aramaki <aramaki@kuicr.kyoto-u.ac.jp>
2019-01-23 12:40:55 +0900

    `--format=mapper` uses `TabularAllHitFormatter`
    Since now `mapper` format outputs all the hits too,
    `mapper-all` renamed to `mapper-one-line`.

--- lib/kohmm/cli/option_parser.rb
+++ lib/kohmm/cli/option_parser.rb
@@ -9,9 +9,9 @@ module KOHMM
             <query>                      FASTA formatted query sequence file
             -o <file>                    File to output the result  [stdout]
             -f, --format <format>        Format of the output [detail]
-              detail:     Detail for each hits (including hits below threshold)
-              mapper:     KEGG Mapper compatible format
-              mapper-all: Similar to mapper, but all hit KO are listed
+              detail:          Detail for each hits (including hits below threshold)
+              mapper:          KEGG Mapper compatible format
+              mapper-one-line: Similar to mapper, but all hit KOs are listed in one line
             --[no-]report-unannotated    Sequence name will be shown even if no KOs are assigned
                                          Default is true when format=mapper or mapper-all,
                                          false when format=detail
@@ -27,9 +27,9 @@ module KOHMM
       end
 
       OUTPUT_FORMATTER_MAP = {
-        "detail"     => OutputFormatter::HitDetailFormatter,
-        "mapper"     => OutputFormatter::SimpleTabularFormatter,
-        "mapper-all" => OutputFormatter::MultiHitTabularFormatter
+        "detail"          => OutputFormatter::HitDetailFormatter,
+        "mapper"          => OutputFormatter::TabularAllHitFormatter,
+        "mapper-one-line" => OutputFormatter::MultiHitTabularFormatter
       }.freeze
       OUTPUT_FORMATTER_MAP.each_key(&:freeze)
 
