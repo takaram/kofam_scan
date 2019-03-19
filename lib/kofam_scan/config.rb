c7c966a9056b9a7315b98e8cff2fa4da372e9f58
Takuya Aramaki <aramaki@kuicr.kyoto-u.ac.jp>
2019-03-19 20:05:49 +0900

    Use require instead of autoload in config.rb

--- lib/kofam_scan/config.rb
+++ lib/kofam_scan/config.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
-autoload :YAML, 'yaml'
-
 module KofamScan
   Config = Struct.new(:output_file, :profile, :ko_list, :e_value, :hmmsearch,
                       :cpu, :tmp_dir, :parallel, :formatter, :reannotation,
                       :create_alignment, :query) do
     def self.load(file)
+      require 'yaml'
       file = File.open(file) if file.kind_of? String
       hash = YAML.safe_load(file)
       file.close
